9c82dac3c736283135c60943f515fa60c91305cc
Ottony Costa <ottony93@gmail.com>
2014-05-10 18:15:37 -0300

    Implements RandonBox#draw

--- app/models/randon_box.rb
+++ app/models/randon_box.rb
@@ -8,6 +8,20 @@ class RandonBox < Box
     @image = generate_randon_image
   end
 
+  def draw window
+    @image.each_with_index do |line, line_index|
+      line.each_with_index do |col, col_index|
+        tile = tile(line_index, col_index)
+
+        window.draw_quad(tile[:A][:x], tile[:A][:y], Gosu::Color.const_get(col),
+                         tile[:B][:x], tile[:B][:y], Gosu::Color.const_get(col),
+                         tile[:C][:x], tile[:C][:y], Gosu::Color.const_get(col),
+                         tile[:D][:x], tile[:D][:y], Gosu::Color.const_get(col))
+
+      end
+    end
+  end
+
   def tile line_index, col_index
     line_position = line_index * @zoom
     col_position = col_index * @zoom
@@ -19,6 +33,7 @@ class RandonBox < Box
       D: { x: @x + col_position + @zoom   , y: @y + line_position + @zoom   },
     }
   end
+
 
   private
 
