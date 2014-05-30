b5851fe17bbc8c03fd8f2af1b3aa3a39a8177e5b
Ottony Costa <ottony93@gmail.com>
2014-05-30 08:18:44 -0300

    Right way to show fps

--- app/models/windows.rb
+++ app/models/windows.rb
@@ -13,19 +13,12 @@ class Windows < Gosu::Window
     @msg = Gosu::Font.new(self, "Times New Roman", 20)
     @randon_box = RandonBox.new window: self
     @time = 0
-    @count = 0
   end
 
   def update
     @randon_box.update
 
     @time += update_interval
-    if ((@time - update_interval).ceil % 1000) == 0
-      @fps = @count
-      @count = 0
-    end
-    @count += 1
-
   end
 
   def controller comb
@@ -39,7 +32,7 @@ class Windows < Gosu::Window
   private def draw_messages
     @msg.draw("X: #{@randon_box.__x__}", 10,640,0,1,1,Gosu::Color::BLUE)
     @msg.draw("Y: #{@randon_box.__y__}", 10,660,0,1,1,Gosu::Color::BLUE)
-    @msg.draw("FPS: #{@fps}", 780,680,0,1,1,Gosu::Color::BLUE)
+    @msg.draw("FPS: #{fps}", 780,680,0,1,1,Gosu::Color::BLUE)
     @msg.draw("Seconds: #{(@time / 1000)}", 10,680,0,1,1,Gosu::Color::BLUE)
   end
 end
